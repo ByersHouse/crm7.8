90433fbfc8372cc74953db44489d6eaf3cffa0b0
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-08-21 10:36:10 +0300

    добавление списка в систему

---
+++ custom/include/language/ru_RU.lang.php
@@ -0,0 +1,8 @@
+<?php
+$GLOBALS['app_list_strings']['leed_status_list']=array (
+  '' => '',
+  'lead' => 'Лид',
+  'client' => 'Клиент',
+  'flagman' => 'Флагман продаж',
+  'vip' => 'V.I.P. Клиент',
+);
