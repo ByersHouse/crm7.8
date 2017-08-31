b195c29480b853d88fce29c8a78c7b29e3663b94
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-08-31 14:35:04 +0300

    автоматический расчет возраста по клику в поле Возраст

--- custom/modules/Leads/js/ledit.js
+++ custom/modules/Leads/js/ledit.js
@@ -1,29 +1,31 @@
 
 
-//$("#age").val(173);
-//$(document).ready(function() {
-    
-    
-    
-    
-  //  $( "#age" ).focus(function() {
-    //       alert("www");
-  //  });
 
-/*
-$( "input[type='text']" ).change(function() {
 
- alert("e");
-
+$( document ).ready(function() {
+    /*
+    $("#date_birthday").input(function() {
+        alert( "wow" );
+    
+    });
+    */
+   
+    $('#age').on('click', function () {
+       //alert( "wow" );
+        var today = new Date();
+        var yr = today.getFullYear();
+        var dr_year = strRight(4,$("#date_birthday").val());
+        $('#age').val(yr-dr_year);
+    });
+ 
+    function strRight(n,str){
+        return str.substring(str.length-n,str.length);
+    }
+    
+    
+    
+    
+    
+    
 });
 
-
-
-
-*/
-
-
-//});
-
-
-('#age').val(999);
