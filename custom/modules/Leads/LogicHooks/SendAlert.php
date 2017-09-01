075547b5f28aa47a59e287a84b046fe5ebe4b77d
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-09-01 17:43:25 +0300

    добавил тип уведомления

--- custom/modules/Leads/LogicHooks/SendAlert.php
+++ custom/modules/Leads/LogicHooks/SendAlert.php
@@ -10,11 +10,16 @@ class LeadHooks{
         
         //var_dump($bean->fetched_row);
         //exit;
+        /*вариант 2 
+         * $alert = BeanFactory::newBean('Alerts');
+            $alert->name = 'My Alert';
+        */
         $seedAlert = new Alert();
         $seedAlert->name = "Предварительный контакт";
         $seedAlert->description = "Вам назначена задача";
         $seedAlert->assigned_user_id = $bean->fetched_row['assigned_user_id'];
         $seedAlert->is_read = 0 ;
+        $seedAlert->type = "info" ;
         $seedAlert->target_module = "Leads";
         $seedAlert->url_redirect = "index.php?action=DetailView&module=Leads&record=".$bean->fetched_row['id']."&return_module=Leads&return_action=DetailView";
         $seedAlert->save();
