c9ac9c720693eeadf91604df41691e77362a6bd8
Yuriy Yurenko <yurenkoyura@gmail.com>
2017-09-04 09:02:09 +0300

    исправил sendalert

--- custom/modules/Leads/LogicHooks/SendAlert.php
+++ custom/modules/Leads/LogicHooks/SendAlert.php
@@ -15,12 +15,12 @@ class LeadHooks{
             $alert->name = 'My Alert';
         */
         $seedAlert = new Alert();
-        $seedAlert->name = "Предварительный контакт";
+        $seedAlert->name = "Предварительный контакт ".$bean->fetched_row['first_name']." ".$bean->fetched_row['last_name'];
         $seedAlert->description = "Вам назначена задача";
         $seedAlert->assigned_user_id = $bean->fetched_row['assigned_user_id'];
         $seedAlert->is_read = 0 ;
         $seedAlert->type = "info" ;
-        $seedAlert->target_module = "Leads";
+        $seedAlert->target_module = "Lead";
         $seedAlert->url_redirect = "index.php?action=DetailView&module=Leads&record=".$bean->fetched_row['id']."&return_module=Leads&return_action=DetailView";
         $seedAlert->save();
         
