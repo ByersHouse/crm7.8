57f15eb3737e2f36d0349c3b656b8e4f117c5aca
nikolay <nikolay.strafe@gmail.com>
2017-09-11 10:21:25 +0300

    Test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if (!defined('sugarEntry')) {
     define('sugarEntry', true);
 }
 /**
- *
+ * NEW NEW NEW
  * SugarCRM Community Edition is a customer relationship management program developed by
  * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
  *
